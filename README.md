432ec7f00e1e74a0693e47ef840b93075f54e0ed
RaveenaBhasin <raveenabhasin15@gmail.com>
2023-12-15 15:15:27 +0530

    finish mint fn interaction

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ npm run dev
 ```
 
 Finally, open [http://localhost:3000](http://localhost:3000) in your browser to view the website.
+
+
+
+Function to get total supply of nft collection
+https://explorer.aptoslabs.com/txn/6996044/changes?network=devnet
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -7,7 +7,8 @@ import Footer from "@/components/Footer";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { DAPP_ADDRESS, APTOS_FAUCET_URL, APTOS_NODE_URL, NETWORK } from "@/config/constants";
 import { AptosClient } from "aptos";
-import { Modal, Input } from "antd";
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 const content = [
   { id: 1, value: '', name: 'Baby Wolfies love $FUR and they are eager to steal them from rabbits. You should try staking your Baby Wolf.' },
@@ -22,6 +23,7 @@ const Forest = () => {
   const [mintTxn, setMintTxn] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [amount, setAmount] = useState(1);
+  const [collectionSupply, setCollectionSupply] = useState(0);
   const client = new AptosClient(APTOS_NODE_URL);
 
   const is_connected = () => {
@@ -37,6 +39,7 @@ const Forest = () => {
   const handleOk = () => {
     setIsModalVisible(false);
     mint_nft();
+    //getMetadata();
   }
 
   const handleCancel = () => {
@@ -48,12 +51,30 @@ const Forest = () => {
     setButtonText('Enter the Forest');
   };
 
+  async function getCollectionSupply() {
+    //TODO
+  }
+
+  async function getMetadata() {
+    is_connected()
+    try {
+      // const res = await client.getAccount(account.address);
+      // const res = await client.view(DAPP_ADDRESS+"::NFTCollection::get_metadata", [], ["Baby Wolfie Token"]);
+      // const res = await client.view(DAPP_ADDRESS+"::NFTCollection::mint_cost", [1]);
+      // const res = await client.getAccountResource(account.address, DAPP_ADDRESS + "::NFTCollection::RabbitPool");
+      // const res = await client.view({function: `${DAPP_ADDRESS}::NFTCollection::baby_wolfie_token_address`, type_arguments: [], arguments: []});
+      const res = await client.view({ function: `${DAPP_ADDRESS}::NFTCollection::get_metadata`, type_arguments: [], arguments: ["Baby Wolfie Token"] });
+      console.log(res);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
   function mint() {
     return {
-      type: "entry_function_payload",
-      function: DAPP_ADDRESS + "::baby_wolfie::mint",
-      type_arguments: [],
-      arguments: [
+      function: `${DAPP_ADDRESS}::NFTCollection::mint`,
+      functionArguments: [
         account.address,
         amount,
       ],
@@ -62,11 +83,16 @@ const Forest = () => {
 
   async function mint_nft() {
     is_connected();
+    console.log(account.address);
     const res = await signAndSubmitTransaction(
-      mint(),
-      { gas_unit_price: 100 }
-    );
-    if(res) {
+      {
+        data: mint(),
+      },
+      {
+        gas_unit_price: 100
+      }
+    )
+    if (res) {
       await client.waitForTransaction(res.hash);
       setMintTxn(res.hash);
       console.log(res.hash);
@@ -77,7 +103,6 @@ const Forest = () => {
     <div className="bg-container">
       <header className="absolute inset-x-0 top-0 z-50">
         <Navbar buttonText={buttonText} handleButtonClick={handleButtonClick} buttonLink="/" />
-        {/* <Navbar/> */}
       </header>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto py-32 lg:py-32">
@@ -100,6 +125,7 @@ const Forest = () => {
                           className="max-w-full h-auto mx-auto border-2 border-gray-800 rounded-md"
                           width={150}
                           height={150}
+                          alt="NFT"
                         />
                       </dd>
                     }
@@ -116,17 +142,71 @@ const Forest = () => {
               Mint my NFT
             </button>
           </div>
-          <Modal
-            title="Enter your Amount"
-            open={isModalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            >
-              <Input type="number" 
-                    value={amount}
-                    onChange={(e) => setAmount(parseInt(e.target.value) || 0)}
-              />
-          </Modal>
+
+          <Transition.Root show={isModalVisible} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={setIsModalVisible}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+
+              <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  >
+                    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                      <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+
+                          <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                            <Dialog.Title as="h2" className="text-xl font-text leading-6 text-gray-900">
+                              Enter the amount of NFTs you want to mint &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                            </Dialog.Title>
+                            <div className="mt-1">
+                              <Input type="number"
+                                value={amount}
+                                onChange={(e) => setAmount(parseInt(e.target.value) || 0)}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                        <button
+                          type="button"
+                          className="inline-flex font-text w-full justify-center rounded-md bg-gray-800 px-3 py-2 text-lg text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto"
+                          onClick={handleOk}
+                        >
+                          Submit
+                        </button>
+                        <button
+                          type="button"
+                          className="mt-3 inline-flex font-text w-full justify-center rounded-md bg-white px-3 py-2 text-lg font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                          onClick={handleCancel}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition.Root>
         </div>
       </div>
       <Footer />
